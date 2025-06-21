470a2aa273318a3edf79caa43a785a5d63e7bb15
Harsh gupta <guptaharsh137@gmail.com>
2025-06-21 18:27:45 +0530

    final push:

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
-import {ScrollTrigger , SplitText} from 'gsap/all';
-import { gsap } from 'gsap';
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import Cocktails from './components/Cocktails.jsx'
+import About from './components/About.jsx'
+import Art from './components/Art.jsx'
+import Menu from './components/Menu.jsx'
+import Contact from './components/Contact.jsx'
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
-function App() {
-  return (
-    <div className='flex-center h-[100vh]'>
-    <h1 className='text-3xl'> APPs</h1>
-    </div>
-  )
+const App = () => {
+ return (
+	<main>
+	 <Navbar />
+	 <Hero />
+	 <Cocktails />
+	 <About />
+	 <Art />
+	 <Menu />
+	 <Contact />
+	</main>
+ )
 }
 
 export default App
